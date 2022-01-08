bf9f85657f4b9144ee4df3cca9f2decb1f7fb8a2
zemzem-svg <zemzemi-k@hotmail.fr>
2022-01-08 13:18:02 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express =require("express")
+const connectDB = require("./config/dbconnect")
+const app = express();
+require('dotenv').config();
+//routes
+app.use(express.json())
+app.use("/user",require("./routes/user"));
+connectDB();
+const PORT = process.env.PORT;
+app.listen(PORT,(err)=>err?console.log(err):console.log(`Server is running well with the port ${PORT}`))
